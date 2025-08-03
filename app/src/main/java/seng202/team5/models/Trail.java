d8b57aa7b23f72f2c3f7fb01a02db4458c4bc8d8
Sam Ladbrook <sla202@uclive.ac.nz>
2025-08-03 14:33:27 +1200

    Added initial code for 'Trail' class

--- app/src/main/java/seng202/team5/models/Trail.java
+++ app/src/main/java/seng202/team5/models/Trail.java
@@ -1,4 +1,56 @@
 package seng202.team5.models;
 
+import java.util.Date;
+
 public class Trail {
+    //base variables from DOC dataset (some may be irrelavent)
+    private int id;
+    private String name;
+    private String description;
+    private String difficulty;
+    private String completionTime;
+    private String hasAlerts;
+    private String thumbnailURL;
+    private String webpageURL;
+    private Date dateLoaded;
+    private int shapeLength;
+
+    //Constructor for base variables
+    public Trail(int id, String name, String difficulty, String description, String completionTime, String hasAlerts, String thumbnailURL, String webpageURL, Date dateloaded, int shapeLength) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.difficulty = difficulty;
+        this.completionTime = completionTime;
+        this.hasAlerts = hasAlerts;
+        this.thumbnailURL = thumbnailURL;
+        this.webpageURL = webpageURL;
+        this.dateLoaded = dateloaded;
+        this.shapeLength = shapeLength;
+    }
+
+    //Getters
+    public int getId() { return id; }
+    public String getName() { return name; }
+    public String getDescription() { return description; }
+    public String getDifficulty() { return difficulty; }
+    public String getCompletionTime() { return completionTime; }
+    public String getHasAlerts() { return hasAlerts; }
+    public String getThumbnailURL() { return thumbnailURL; }
+    public String getWebpageURL() { return webpageURL; }
+    public Date getDateLoaded() { return dateLoaded; }
+    public int getShapeLength() { return shapeLength; }
+
+    //Setters
+    public void setId(int id) { this.id = id; }
+    public void setName(String name) { this.name = name; }
+    public void setDescription(String description) { this.description = description; }
+    public void setDifficulty(String difficulty) { this.difficulty = difficulty; }
+    public void setCompletionTime(String completionTime) { this.completionTime = completionTime; }
+    public void setHasAlerts(String hasAlerts) { this.hasAlerts = hasAlerts; }
+    public void setThumbnailURL(String thumbnailURL) { this.thumbnailURL = thumbnailURL; }
+    public void setWebpageURL(String webpageURL) { this.webpageURL = webpageURL; }
+    public void setDateLoaded(Date dateLoaded) { this.dateLoaded = dateLoaded; }
+    public void setShapeLength(int shapeLength) { this.shapeLength = shapeLength; }
+
 }
