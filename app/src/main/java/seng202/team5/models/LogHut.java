bf1f386072bebd6a6f63689b932a503b52ec32ef
Sam <sla202@uclive.ac.nz>
2025-08-04 20:39:00 +1200

    Added initial code for 'LogHut' class

--- app/src/main/java/seng202/team5/models/LogHut.java
+++ app/src/main/java/seng202/team5/models/LogHut.java
@@ -1,4 +1,34 @@
 package seng202.team5.models;
 
+import java.util.Date;
+
+//Some basic variables
 public class LogHut {
+    private int id;
+    private User user;
+    private Hut hut;
+    private Date dateVisited;
+    private String notes;
+
+    //Constructor for base variables
+    public LogHut(int id, User user, Hut hut, Date dateVisited, String notes) {
+        this.id = id;
+        this.user = user;
+        this.hut = hut;
+        this.dateVisited = dateVisited;
+        this.notes = notes;
+    }
+
+    //getters
+    public int getId() { return id; }
+    public User getUser() { return user; }
+    public Hut getHut() { return hut; }
+    public Date getDateVisited() { return dateVisited; }
+    public String getNotes() { return notes; }
+
+    //setters
+    public void setUser(User user) { this.user = user; }
+    public void setHut(Hut hut) { this.hut = hut; }
+    public void setDateVisited(Date dateVisited) { this.dateVisited = dateVisited; }
+    public void setNotes(String notes) { this.notes = notes; }
 }
