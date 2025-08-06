096a02eb3bd107ba4496731296206508514654dc
Sam Ladbrook <sla202@uclive.ac.nz>
2025-08-06 14:36:26 +1200

    Setup basic code for 'DataService class'

--- app/src/main/java/seng202/team5/services/DataService.java
+++ app/src/main/java/seng202/team5/services/DataService.java
@@ -1,4 +1,19 @@
 package seng202.team5.services;
 
+import seng202.team5.data.FileBasedTrailRepo;
+import seng202.team5.models.Trail;
+
+import java.util.List;
+
 public class DataService {
+
+    private FileBasedTrailRepo trailRepo;
+
+    public DataService(String trailCSVPath) {
+        trailRepo = new FileBasedTrailRepo(trailCSVPath);
+    }
+
+    public List<Trail> getTrails() {
+        return trailRepo.getAllTrails();
+    }
 }
