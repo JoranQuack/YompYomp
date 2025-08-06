1b02dfb57e9ed2e1c4281eab2191a1f660461e16
Sam Ladbrook <sla202@uclive.ac.nz>
2025-08-06 14:52:59 +1200

    Wrote very basic code for 'ITrail' interface

--- app/src/main/java/seng202/team5/data/ITrail.java
+++ app/src/main/java/seng202/team5/data/ITrail.java
@@ -1,4 +1,15 @@
 package seng202.team5.data;
 
+import seng202.team5.models.Trail;
+import java.util.List;
+
+/**
+ * Interface for accessing the trail data
+ */
 public interface ITrail {
+    /**
+     * Returns all trails avaliable in the data source
+     * @return List of the trail objects
+     */
+    List<Trail> getAllTrails();
 }
