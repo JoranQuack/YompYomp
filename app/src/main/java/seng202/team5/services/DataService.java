e8bf7a608348f7e1bff4ceb3f457b1de1f3f0d9b
Sam Ladbrook <sla202@uclive.ac.nz>
2025-08-06 14:58:32 +1200

    Made sure 'DataService' uses 'ITrail' interface

--- app/src/main/java/seng202/team5/services/DataService.java
+++ app/src/main/java/seng202/team5/services/DataService.java
@@ -2,15 +2,15 @@ package seng202.team5.services;
 
 import seng202.team5.data.FileBasedTrailRepo;
 import seng202.team5.models.Trail;
+import seng202.team5.data.ITrail;
 
 import java.util.List;
 
 public class DataService {
-
-    private FileBasedTrailRepo trailRepo;
+    private final ITrail trailRepo;
 
     public DataService(String trailCSVPath) {
-        trailRepo = new FileBasedTrailRepo(trailCSVPath);
+        this.trailRepo = new FileBasedTrailRepo(trailCSVPath);
     }
 
     public List<Trail> getTrails() {
