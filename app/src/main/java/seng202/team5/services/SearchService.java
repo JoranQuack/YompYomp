9d5f8585e1b8e1d36748c5896fb79d795d8778fe
Joran Le Quellec <jle209@uclive.ac.nz>
2025-08-06 21:33:10 +1200

    refactor: general UI tweaks and Thumbnail and Link functions

--- app/src/main/java/seng202/team5/services/SearchService.java
+++ app/src/main/java/seng202/team5/services/SearchService.java
@@ -9,6 +9,7 @@ public class SearchService {
     // private static SearchService instance;
     private static DataService dataService;
     private List<Trail> trails;
+    private static final int MAX_RESULTS = 50;
 
     public SearchService() {
         String currentPath = System.getProperty("user.dir");
@@ -18,14 +19,23 @@ public class SearchService {
         trails = dataService.getTrails();
     }
 
+    /**
+     * Searches for trails based on the provided query. If the query is null or
+     * empty, it returns the first 100 trails.
+     *
+     * @param query The search query to filter trails by name
+     * @return A list of trails matching the search query, limited to 100 results
+     *
+     */
     public List<Trail> searchTrails(String query) {
         if (query == null || query.isEmpty()) {
-            return trails; // Return all trails if no query
+            return trails.stream().limit(MAX_RESULTS).collect(Collectors.toList()); // Return all trails if no query
         }
 
         // Filter trails based on the search query
         return trails.stream()
                 .filter(trail -> trail.getName().toLowerCase().contains(query.toLowerCase()))
+                .limit(MAX_RESULTS)
                 .collect(Collectors.toList());
     }
 
