2756340e3c074b2583ba37b193bc6d9c508bc323
Sam Ladbrook <sla202@uclive.ac.nz>
2025-08-06 16:52:02 +1200

    Altered 'Environment' class to use CSV reader and store the trails list

--- app/src/main/java/seng202/team5/Environment.java
+++ app/src/main/java/seng202/team5/Environment.java
@@ -1,10 +1,18 @@
 package seng202.team5;
 
 import seng202.team5.gui.ScreenNavigator;
+import seng202.team5.models.Trail;
+import seng202.team5.services.DataService;
+
+import java.net.URL;
+import java.util.List;
 
 public class Environment {
 
     private final ScreenNavigator navigator;
+
+    //Created a dataservice variable
+    private static DataService dataService;
 
     /**
      * Constructor for the GameEnvironment class. Initializes the game environment
@@ -15,6 +23,15 @@ public class Environment {
      */
     public Environment(ScreenNavigator navigator) {
         this.navigator = navigator;
+
+        //temporary --------
+        String currentPath = System.getProperty("user.dir");
+        String fullPath = currentPath + "/app/src/main/resources/datasets/DOC_Walking_Experiences_7994760352369043452.csv";
+        dataService = new DataService(fullPath);
+        //trails list :)
+        List<Trail> trails = dataService.getTrails();
+        //------------------
+
         navigator.launchStartScreen(this);
     }
 
