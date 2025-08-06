5672359fd7a156941dd8c6834ada35bdfbe7b2be
Joran Le Quellec <jle209@uclive.ac.nz>
2025-08-06 20:40:59 +1200

    feat: Searching through trails

--- app/src/main/java/seng202/team5/gui/TrailsController.java
+++ app/src/main/java/seng202/team5/gui/TrailsController.java
@@ -5,7 +5,6 @@ import java.util.List;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-// import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 import seng202.team5.models.Trail;
@@ -30,8 +29,17 @@ public class TrailsController extends Controller {
     @FXML
     private void initialize() {
         List<Trail> trails = searchService.searchTrails(null);
+        updateTrailsGrid(trails);
+    }
 
-        // Populate the trails grid with trail data
+    private void clearTrailsGrid() {
+        trailsGridPane.getChildren().clear();
+        trailsGridPane.addRow(0, new Label("Name"), new Label("Thumbnail URL"), new Label("Description"),
+                new Label("Difficulty"), new Label("Completion Time"), new Label("Webpage URL"));
+    }
+
+    private void updateTrailsGrid(List<Trail> trails) {
+        clearTrailsGrid();
         for (int i = 0; i < trails.size(); i++) {
             Trail trail = trails.get(i);
             trailsGridPane.addRow(i + 1,
@@ -42,6 +50,13 @@ public class TrailsController extends Controller {
                     new Label(trail.getCompletionTime()),
                     new Label(trail.getWebpageURL()));
         }
+    }
+
+    @FXML
+    private void onSearchButtonClicked() {
+        String query = searchBarTextField.getText();
+        List<Trail> filteredTrails = searchService.searchTrails(query);
+        updateTrailsGrid(filteredTrails);
     }
 
     @Override
