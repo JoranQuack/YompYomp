d760ea918685832ff5af22c8d67bfa95f7d3d398
Joran Le Quellec <jle209@uclive.ac.nz>
2025-08-07 17:39:09 +1200

    refactor: Remove initial test code

--- app/src/test/java/seng202/team5/AppTest.java
+++ app/src/test/java/seng202/team5/AppTest.java
@@ -1,45 +1,15 @@
 package seng202.team5;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+// import org.junit.jupiter.api.BeforeEach;
+// import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+// import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class AppTest {
-    private App app;
+    // private App app;
 
-    @BeforeEach
-    public void setUp() {
-        app = new App();
-    }
-
-    @Test
-    public void testHelloWorld() {
-        assertEquals("Hello World", app.helloWorld());
-    }
-
-    @Test
-    public void testTeamName() {
-        assertEquals("The Dream Team 5", app.teamName());
-    }
-
-    @Test
-    public void testHayley() {
-        assertEquals("Hello Hayley", app.helloHayley());
-    }
-
-    @Test
-    public void testAppName() {
-        assertEquals("YompYomp", app.appName());
-    }
-
-    @Test
-    public void testYellowCard() {
-        assertEquals("Bring snacks", app.yellowCard());
-    }
-
-    @Test
-    public void testRedCard() {
-        assertEquals("Your Out", app.redCard());
-    }
+    // @BeforeEach
+    // public void setUp() {
+    // app = new App();
+    // }
 }
