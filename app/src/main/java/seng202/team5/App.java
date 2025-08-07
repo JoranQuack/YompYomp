c2d2a4eeab0a359b38ab44695d7cad7932b04ce0
Joran Le Quellec <jle209@uclive.ac.nz>
2025-08-07 17:39:09 +1200

    refactor: Remove initial test code

--- app/src/main/java/seng202/team5/App.java
+++ app/src/main/java/seng202/team5/App.java
@@ -3,30 +3,6 @@ package seng202.team5;
 import seng202.team5.gui.FXAppEntry;
 
 public class App {
-    public String helloWorld() {
-        return "Hello World";
-    }
-
-    public String teamName() {
-        return "The Dream Team 5";
-    }
-
-    public String helloHayley() {
-        return "Hello Hayley";
-    }
-
-    public String appName() {
-        return "YompYomp";
-    }
-
-    public String yellowCard() {
-        return "Bring snacks";
-    }
-
-    public String redCard() {
-        return "Your Out";
-    }
-
     public static void main(String[] args) {
         FXAppEntry.launch(FXAppEntry.class, args);
     }
