feedd0c5328e19a9846b7b097c9b3e0813c33959
Sienna Robinson <sro179@uclive.ac.nz>
2025-08-18 13:17:29 +1200

    Added the filePath for the keyword CSV and set up the constructor and called it from searchService.

--- app/src/main/java/seng202/team5/services/DataService.java
+++ app/src/main/java/seng202/team5/services/DataService.java
@@ -1,10 +1,13 @@
 package seng202.team5.services;
 
+import seng202.team5.data.FileBasedKeywordRepo;
 import seng202.team5.data.FileBasedTrailRepo;
+import seng202.team5.data.IKeyword;
 import seng202.team5.models.Trail;
 import seng202.team5.data.ITrail;
 
 import java.util.List;
+import java.util.Map;
 
 /**
  * Service class for managing trail data operations.
@@ -14,13 +17,26 @@ public class DataService {
     /** Trail repository for data access */
     private final ITrail trailRepo;
 
+    /** Keyword repository for data access */
+    private final IKeyword keywordRepo;
+
+    /** List of all trails */
+    private final List<Trail> trails;
+
+    /** Categories of keywords loaded from CSV file */
+    private final Map<String, List<String>> keywords;
+
     /**
-     * Creates DataService with CSV file path.
+     * Creates DataService with CSV file pathes.
      *
      * @param trailCSVPath Path to trail CSV file
+     * @param keywordCSVPath Path to the keyword CSV file
      */
-    public DataService(String trailCSVPath) {
+    public DataService(String trailCSVPath, String keywordCSVPath) {
         this.trailRepo = new FileBasedTrailRepo(trailCSVPath);
+        this.keywordRepo = new FileBasedKeywordRepo(keywordCSVPath);
+        this.trails = trailRepo.getAllTrails();
+        this.keywords = keywordRepo.getKeywords();
     }
 
     /**
@@ -30,5 +46,14 @@ public class DataService {
      */
     public List<Trail> getTrails() {
         return trailRepo.getAllTrails();
+    }
+
+    /**
+     * Gets the map of categories to keywords.
+     *
+     * @return The map of categories to keywords.
+     */
+    public Map<String, List<String>> getKeywords() {
+        return keywordRepo.getKeywords();
     }
 }
