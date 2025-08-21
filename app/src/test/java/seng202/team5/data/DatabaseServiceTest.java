3065453314ac6304c7874d2cc27efce1d03c4616
Joran Le Quellec <jle209@uclive.ac.nz>
2025-08-21 14:57:43 +1200

    feat: Begin DatabaseService testing

---
+++ app/src/test/java/seng202/team5/data/DatabaseServiceTest.java
@@ -0,0 +1,8 @@
+package seng202.team5.data;
+
+public class DatabaseServiceTest {
+    public void getConnectionTest() {
+        DatabaseService databaseService = new DatabaseService("database/test.db");
+
+    }
+}
