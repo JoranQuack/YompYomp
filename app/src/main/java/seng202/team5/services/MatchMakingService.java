b731f2ad413441c2d79ce24e43c3db5ecff9cb80
Hayley Kawelenga <hka105@uclive.ac.nz>
2025-08-27 20:03:53 +1200

    Added method setUserPreferences for mapping user answers to a category based on keywords

--- app/src/main/java/seng202/team5/services/MatchMakingService.java
+++ app/src/main/java/seng202/team5/services/MatchMakingService.java
@@ -1,6 +1,7 @@
 package seng202.team5.services;
 
 import seng202.team5.data.IKeyword;
+import seng202.team5.models.User;
 
 import java.util.*;
 
@@ -15,5 +16,24 @@ public class MatchMakingService {
 
     public MatchMakingService(IKeyword keywordRepo) {
         this.keywordCategories = keywordRepo.getKeywords();
+    }
+
+    /**
+     * Sets the user preferences from a User object.
+     * Each preference is maps to a keyword category
+     * @param user the user whose preferences will be mapped
+     */
+    public void setUserPreferences(User user) {
+        userWeights.clear();
+
+        //Yes/No simplified to 0 or 5
+        userWeights.put("FamilyFriendly", user.getIsFamilyFriendly() ?5:0);
+        userWeights.put("Accessible", user.getIsAccessible() ?5:0);
+
+        userWeights.put("Difficult", user.getExperienceLevel());
+        userWeights.put("Rocky", user.getGradientPreference());
+        userWeights.put("Forest", user.getBushPreference());
+        userWeights.put("Wet", user.getLakeRiverPreference());
+         //TODO: add more categories
     }
 }
