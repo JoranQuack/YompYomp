f6f035d3c99ae53cd2b9f1d0fd6b9bffdb097e2c
Sam <sla202@uclive.ac.nz>
2025-08-30 15:37:36 +1200

    Added background worker in environment to handle database sync and scraping images

--- app/src/main/java/seng202/team5/Environment.java
+++ app/src/main/java/seng202/team5/Environment.java
@@ -1,11 +1,24 @@
 package seng202.team5;
 
+import javafx.application.Platform;
+import javafx.concurrent.Task;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 import seng202.team5.gui.ScreenNavigator;
 import seng202.team5.gui.WelcomeController;
+import seng202.team5.data.FileBasedTrailRepo;
+import seng202.team5.data.SqlBasedTrailRepo;
+import seng202.team5.data.DatabaseService;
+import seng202.team5.services.SetupService;
 
 public class Environment {
 
     private final ScreenNavigator navigator;
+
+    //background worker + setup
+    private final ExecutorService setupExec;
+    private final SetupService setupService;
 
     /**
      * Constructor for the Environment class. Initializes the environment
@@ -15,7 +28,51 @@ public class Environment {
      */
     public Environment(ScreenNavigator navigator) {
         this.navigator = navigator;
-        navigator.launchScreen(new WelcomeController(this, navigator));
+
+        DatabaseService dbService = new DatabaseService();
+        SqlBasedTrailRepo sqlTrailRepo = new SqlBasedTrailRepo(dbService);
+        FileBasedTrailRepo fileTrailRepo = new FileBasedTrailRepo("/datasets/DOC_Walking_Experiences_7994760352369043452.csv");
+        this.setupService = new SetupService(sqlTrailRepo, fileTrailRepo);
+
+
+        WelcomeController welcome = new WelcomeController(this, navigator);
+        navigator.launchScreen(welcome);
+
+        this.setupExec = Executors.newSingleThreadExecutor(r -> {
+            Thread thread = new Thread(r, "setup-worker");
+            thread.setDaemon(true);
+            return thread;
+        });
+
+        runSetupInBackground(welcome);
+    }
+
+    private void runSetupInBackground(WelcomeController welcomeController) {
+        Task<Void> setupTask = new Task<>() {
+            @Override protected Void call() throws Exception {
+                updateMessage("Starting setup...");
+                setupService.setupApplication();
+
+                return null;
+            }
+        };
+
+        // TODO: add setOnFailed methods
+
+        setupTask.setOnSucceeded(e -> {
+            Platform.runLater(() -> {
+                System.out.println("Background worker completed");
+            });
+        });
+
+        setupExec.submit(setupTask);
+    }
+
+    /**
+     * This method needs to be called from the app shutdown hook to ensure background worker is shut down
+     */
+    public void shutdown() {
+        setupExec.shutdownNow();
     }
 
     /**
