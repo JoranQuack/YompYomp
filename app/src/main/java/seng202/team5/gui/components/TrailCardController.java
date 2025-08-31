40c906044bfe55133e0cab90687919fa5ac3e0c1
Joran Le Quellec <jle209@uclive.ac.nz>
2025-08-31 17:51:14 +1200

    Refactor TrailCardController to use ImageService

--- app/src/main/java/seng202/team5/gui/components/TrailCardController.java
+++ app/src/main/java/seng202/team5/gui/components/TrailCardController.java
@@ -1,11 +1,16 @@
 package seng202.team5.gui.components;
 
 import java.io.IOException;
-import javafx.fxml.*;
-import javafx.scene.control.*;
-import javafx.scene.image.*;
-import javafx.scene.layout.*;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Label;
+import javafx.scene.control.ProgressBar;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.VBox;
 import seng202.team5.models.Trail;
+import seng202.team5.services.ImageService;
 
 public class TrailCardController extends VBox {
 
@@ -25,7 +30,10 @@ public class TrailCardController extends VBox {
     @FXML
     private Label regionLabel;
 
+    private final ImageService imageService;
+
     public TrailCardController() {
+        this.imageService = new ImageService();
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/components/trail_card.fxml"));
         fxmlLoader.setRoot(this);
         fxmlLoader.setController(this); // FXML elements
@@ -37,22 +45,18 @@ public class TrailCardController extends VBox {
     }
 
     /**
+     * Sets the trail data for this card component.
      *
-     * @param trail
+     * @param trail The trail object to display
      */
     public void setData(Trail trail) {
         title.setText(trail.getName());
 
-        try {
-            Image image = new Image(trail.getThumbnailURL(), true);
-            thumbnail.setImage(image);
-        } catch (RuntimeException e) {
-            thumbnail.setImage(null);
-        }
+        Image trailImage = imageService.loadTrailImage(trail.getThumbnailURL());
+        thumbnail.setImage(trailImage);
 
         difficultyLabel.setText(trail.getDifficulty());
         durationLabel.setText(trail.getType());
         regionLabel.setText("Region");
     }
-
 }
