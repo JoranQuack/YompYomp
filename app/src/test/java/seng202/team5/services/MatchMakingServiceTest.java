4a9915907ae3b46afe62c85586504bd3a982ee0b
Hayley Kawelenga <hka105@uclive.ac.nz>
2025-09-02 09:47:58 +1200

    Added PerfectMatch unit test

--- app/src/test/java/seng202/team5/services/MatchMakingServiceTest.java
+++ app/src/test/java/seng202/team5/services/MatchMakingServiceTest.java
@@ -19,10 +19,18 @@ class MatchMakingServiceTest {
     void setUp() {
         IKeyword fakeKeywordRepo = () -> {
             Map<String, List<String>> map = new HashMap<>();
+            map.put("FamilyFriendly", Arrays.asList("children", "easy"));
+            map.put("Accessible", Arrays.asList("accessible", "abilities"));
+            map.put("Difficult", Arrays.asList("difficult", "challenging"));
+            map.put("Rocky", Arrays.asList("steep", "gorge"));
+            map.put("Reserve", Arrays.asList("reserve", "park"));
             map.put("Wet", Arrays.asList("lake", "river"));
             map.put("Forest", Arrays.asList("forest", "bush"));
+            map.put("Coast", Arrays.asList("coast", "beach"));
+            map.put("Wildlife", Arrays.asList("wildlife", "animal"));
             map.put("Alpine", Arrays.asList("mountain", "hill"));
             map.put("Historical", Arrays.asList("historic-site", "ruins"));
+            map.put("Waterfall", Arrays.asList("waterfall", "falls"));
             return map;
         };
 
@@ -69,5 +77,18 @@ class MatchMakingServiceTest {
 
         double score = matchMakingService.scoreTrail(trailKeywords);
         assertEquals(0.0, score, 0.0001);
+    }
+
+    @Test
+    @DisplayName("Perfect match should return 100%")
+    void TestPerfectMatch() {
+        User user = makeTestUser();
+        matchMakingService.setUserPreferences(user);
+
+        //trail contains all keywords in repo
+        List<String> trailKeywords =  Arrays.asList("lake", "forest", "mountain", "ruins", "children", "accessible", "difficult", "steep", "reserve", "coast", "wildlife", "waterfall");
+
+        double score = matchMakingService.scoreTrail(trailKeywords);
+        assertEquals(1.0, score, 0.0001);
     }
 }
