e636f977b90cb95c0b468bb7f72a07c1a447e5e7
Joran Le Quellec <jle209@uclive.ac.nz>
2025-09-04 15:53:16 +1200

    Implement UserService class with methods to get and set the current user, added todos for future work

--- app/src/main/java/seng202/team5/services/UserService.java
+++ app/src/main/java/seng202/team5/services/UserService.java
@@ -1,4 +1,56 @@
 package seng202.team5.services;
 
+import javax.management.Query;
+
+import seng202.team5.data.DatabaseService;
+import seng202.team5.data.QueryHelper;
+import seng202.team5.models.User;
+
 public class UserService {
+    User user;
+
+    /**
+     * Constructor for UserService
+     */
+    public UserService() {
+        this.user = null;
+    }
+
+    /**
+     * Get the current user.
+     *
+     * @return the current user
+     */
+    public User getUser() {
+        if (user == null) {
+            user = loadUserFromDatabase();
+        }
+        return user;
+    }
+
+    /**
+     * Load the user from the database.
+     *
+     * @return the user loaded from the database, or guest user if none found
+     */
+    private User loadUserFromDatabase() {
+        DatabaseService dbService = new DatabaseService();
+        QueryHelper queryHelper = new QueryHelper(dbService);
+
+        // TODO: load user from database
+    }
+
+    /**
+     * Set the current user, and updates it in the database if needed.
+     *
+     * @param user the user to set
+     */
+    public void setUser(User user) {
+        this.user = user;
+
+        DatabaseService dbService = new DatabaseService();
+
+        // TODO: update user in database if needed
+
+    }
 }
