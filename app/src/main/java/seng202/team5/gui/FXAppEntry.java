0dd7e64096589b272ed2d79b8bc36b107f59d1e5
Joran Le Quellec <jle209@uclive.ac.nz>
2025-09-06 11:31:07 +1200

    Move background setup functionality into FXAppEntry

--- app/src/main/java/seng202/team5/gui/FXAppEntry.java
+++ app/src/main/java/seng202/team5/gui/FXAppEntry.java
@@ -1,20 +1,29 @@
 package seng202.team5.gui;
 
 import java.util.Objects;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.concurrent.Task;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
-import seng202.team5.Environment;
+import seng202.team5.data.DatabaseService;
+import seng202.team5.data.FileBasedTrailRepo;
+import seng202.team5.data.SqlBasedTrailRepo;
+import seng202.team5.services.SetupService;
 
 /**
  * Class that starts the JavaFX application thread.
  */
 public class FXAppEntry extends Application {
 
+    private ExecutorService setupExec;
+
     /**
-     * Creates the {@link Environment} with a {@link ScreenNavigator} for the
-     * given {@link Stage}
+     * Creates the application with a {@link ScreenNavigator} for the
+     * given {@link Stage} and handles background setup
      *
      * @param primaryStage The current fxml stage, handled by this JavaFX
      *                     Application class
@@ -26,6 +35,60 @@ public class FXAppEntry extends Application {
         primaryStage.setTitle("YompYomp");
         primaryStage.show();
 
-        new Environment(new ScreenNavigator(primaryStage));
+        ScreenNavigator navigator = new ScreenNavigator(primaryStage);
+        WelcomeController welcome = new WelcomeController(navigator);
+        navigator.launchScreen(welcome);
+
+        // Initialise background setup
+        setupExec = Executors.newSingleThreadExecutor(r -> {
+            Thread thread = new Thread(r, "setup-worker");
+            thread.setDaemon(true);
+            return thread;
+        });
+
+        runSetupInBackground(welcome);
+    }
+
+    /**
+     * This method is used to start the setup of the application on the second
+     * thread
+     *
+     * @param welcomeController the welcome controller to potentially update
+     */
+    private void runSetupInBackground(WelcomeController welcomeController) {
+        Task<Void> setupTask = new Task<>() {
+            @Override
+            protected Void call() throws Exception {
+                updateMessage("Starting setup...");
+
+                DatabaseService dbService = new DatabaseService();
+                SqlBasedTrailRepo sqlTrailRepo = new SqlBasedTrailRepo(dbService);
+                FileBasedTrailRepo fileTrailRepo = new FileBasedTrailRepo(
+                        "/datasets/DOC_Walking_Experiences_7994760352369043452.csv");
+                SetupService setupService = new SetupService(sqlTrailRepo, fileTrailRepo);
+
+                setupService.setupApplication();
+                return null;
+            }
+        };
+
+        // Handle setup completion
+        setupTask.setOnSucceeded(e -> {
+            Platform.runLater(() -> {
+                System.out.println("Background worker completed");
+            });
+        });
+
+        setupExec.submit(setupTask);
+    }
+
+    /**
+     * This method is called when the application is shutting down
+     */
+    @Override
+    public void stop() {
+        if (setupExec != null) {
+            setupExec.shutdownNow();
+        }
     }
 }
