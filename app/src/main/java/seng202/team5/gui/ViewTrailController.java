5a74927071f9401e078834e200ade75e39c4e5df
Lam Tran <ltr53@uclive.ac.nz>
2025-09-15 17:13:05 +1200

    initialise all components through controller

--- app/src/main/java/seng202/team5/gui/ViewTrailController.java
+++ app/src/main/java/seng202/team5/gui/ViewTrailController.java
@@ -4,11 +4,14 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ProgressBar;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import seng202.team5.models.Trail;
+import seng202.team5.services.ImageService;
 
 public class ViewTrailController extends Controller {
 
+    private final ImageService imageService;
     private Trail trail;
 
     /**
@@ -18,6 +21,7 @@ public class ViewTrailController extends Controller {
      */
     public ViewTrailController(ScreenNavigator navigator, Trail trail) {
         super(navigator);
+        this.imageService = new ImageService();
         this.trail = trail;
     }
 
@@ -37,11 +41,22 @@ public class ViewTrailController extends Controller {
     private ImageView trailMap;
     @FXML
     private ImageView trailThumbnail;
+    @FXML
+    private Button backButton;
     // hut labels and images currently invisible
 
     @FXML
     private void initialize() {
+        trailNameLabel.setText(trail.getName());
 
+        Image trailImage = imageService.loadTrailImage(trail.getThumbnailURL());
+        trailThumbnail.setImage(trailImage);
+        double weight = trail.getUserWeight();
+        matchBar.setProgress(weight);
+        int matchPercent = (int) Math.round(weight * 100);
+        matchLabel.setText(matchPercent + "% match");
+        descriptionLabel.setText(trail.getDescription());
+        // TODO: add icon for map
     }
 
     @Override
