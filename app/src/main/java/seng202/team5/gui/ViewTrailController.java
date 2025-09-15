c1f0452416e81188fcf943da09c48e2cdf923512
Lam Tran <ltr53@uclive.ac.nz>
2025-09-15 16:21:23 +1200

    hooked up fxml components to controller class objects

--- app/src/main/java/seng202/team5/gui/ViewTrailController.java
+++ app/src/main/java/seng202/team5/gui/ViewTrailController.java
@@ -1,5 +1,11 @@
 package seng202.team5.gui;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ProgressBar;
+import javafx.scene.image.ImageView;
+
 public class ViewTrailController extends Controller {
 
     /**
@@ -10,6 +16,24 @@ public class ViewTrailController extends Controller {
     public ViewTrailController(ScreenNavigator navigator) {
         super(navigator);
     }
+
+    @FXML
+    private Label trailNameLabel;
+    @FXML
+    private Label translationLabel;
+    @FXML
+    private Label matchLabel;
+    @FXML
+    private ProgressBar matchBar;
+    @FXML
+    private Label descriptionLabel;
+    @FXML
+    private Button editInfoButton;
+    @FXML
+    private ImageView trailMap;
+    @FXML
+    private ImageView trailThumbnail;
+    // hut labels and images currently invisible
 
     @Override
     protected String getFxmlFile() {
