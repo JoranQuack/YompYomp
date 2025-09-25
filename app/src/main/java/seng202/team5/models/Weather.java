cb661f15405f9794978e4d5729af957513e01460
Jonny Chan <jch485@uclive.ac.nz>
2025-09-25 18:20:37 +1200

    Declared variables and created methods of Weather.java

--- app/src/main/java/seng202/team5/models/Weather.java
+++ app/src/main/java/seng202/team5/models/Weather.java
@@ -1,4 +1,44 @@
 package seng202.team5.models;
 
 public class Weather {
+    private double latitude;
+    private double longitude;
+    private double temperature;
+    private double tempMax;
+    private double tempMin;
+    private String location;
+
+    public Weather(double latitude, double longitude, double temperature, double tempMax, double tempMin, String location) {
+        this.latitude = latitude;
+        this.longitude = longitude;
+        this.temperature = temperature;
+        this.tempMax = tempMax;
+        this.tempMin = tempMin;
+        this.location = location;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public double getTemperature() {
+        return temperature;
+    }
+
+    public double getTempMax() {
+        return tempMax;
+    }
+
+    public double getTempMin() {
+        return tempMin;
+    }
+
+    public String getLocation() {
+        return location;
+    }
 }
+
+
