15cb86fb7d792d8a09aa0aa555677dc1432b0243
Hayley Kawelenga <hka105@uclive.ac.nz>
2025-09-25 20:44:25 +1200

    Created JavaScriptBridge for map.html

---
+++ app/src/main/java/seng202/team5/gui/JavaScriptBridge.java
@@ -0,0 +1,22 @@
+package seng202.team5.gui;
+
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
+import java.text.ParseException;
+
+public class JavaScriptBridge {
+
+    public void addMarkerFromClick(String latlng){
+        JSONParser parser = new JSONParser();
+        try {
+            JSONObject latlng_json = (JSONObject) parser.parse(latlng);
+            float lat = ((Double)latlng_json.get("lat")).floatValue();
+            float lng = ((Double) latlng_json.get("lng")).floatValue();
+            String logMessage = String.format("From Java: you clicked at %s, %s", lat, lng);
+            System.out.println(logMessage);
+        } catch (org.json.simple.parser.ParseException e) {
+            e.printStackTrace();
+        }
+    }
+}
