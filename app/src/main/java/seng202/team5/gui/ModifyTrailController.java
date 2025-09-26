df92f6e3e7519bcdb2de059a18d12663bae41901
Hayley Kawelenga <hka105@uclive.ac.nz>
2025-09-26 15:54:03 +1200

    Added methods necessary to load map on Modify trail screen

--- app/src/main/java/seng202/team5/gui/ModifyTrailController.java
+++ app/src/main/java/seng202/team5/gui/ModifyTrailController.java
@@ -1,9 +1,14 @@
 package seng202.team5.gui;
 
+import com.sun.javafx.webkit.WebConsoleListener;
+import javafx.concurrent.Worker;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
 import javafx.scene.image.ImageView;
 import javafx.scene.paint.Color;
+import javafx.scene.web.WebEngine;
+import javafx.scene.web.WebView;
+import netscape.javascript.JSObject;
 import seng202.team5.data.DatabaseService;
 import seng202.team5.data.SqlBasedTrailRepo;
 import seng202.team5.models.Trail;
@@ -22,6 +27,11 @@ public class ModifyTrailController extends Controller {
     private Controller lastController;
     private DatabaseService databaseService;
     private SqlBasedTrailRepo sqlBasedTrailRepo;
+
+    private WebEngine webEngine;
+    private JavaScriptBridge javaScriptBridge;
+    private JSObject javaScriptConnector;
+//    private Geolocator geolocator;
 
     /**
      * Launches the screen with navigator
@@ -56,7 +66,7 @@ public class ModifyTrailController extends Controller {
     @FXML
     private ComboBox<String> regionComboBox;
     @FXML
-    private ImageView mapImage;
+    private WebView trailMapView;
     @FXML
     private Label emptyFieldLabel;
     @FXML
@@ -89,6 +99,37 @@ public class ModifyTrailController extends Controller {
         emptyFieldLabel.setText("");
         saveButton.setOnAction(e -> onSaveButtonClicked());
         backButton.setOnAction(e -> onBackButtonClicked());
+
+        // Map methods below
+        javaScriptBridge = new JavaScriptBridge();
+        initMap();
+    }
+
+    /**
+     * Initialises the WebView loading in the appropriate html and initialising important communicator
+     * objects between Java and Javascript
+     */
+    private void initMap() {
+        webEngine = trailMapView.getEngine();
+        webEngine.setJavaScriptEnabled(true);
+        webEngine.load(Controller.class.getResource("/html/map.html").toExternalForm());
+        //Forwards console.log() output from any javascript to info log
+        WebConsoleListener.setDefaultListener((view, message, lineNumber, sourceID) ->
+                System.out.printf(String.format("Map WebView console log line: %d, message : %s", lineNumber, message)));
+
+        webEngine.getLoadWorker().stateProperty().addListener(
+                (ov, oldState, newState) -> {
+                    // if javascript loads successfully
+                    if (newState == Worker.State.SUCCEEDED) {
+                        // set our bridge object
+                        JSObject window = (JSObject) webEngine.executeScript("window");
+                        window.setMember("javaScriptBridge", javaScriptBridge);
+                        // get a reference to the js object that has a reference to the js methods we need to use in java
+                        javaScriptConnector = (JSObject) webEngine.executeScript("jsConnector");
+                        // call the javascript function to initialise the map
+                        javaScriptConnector.call("initMap", -44.0, 171.0); // arbitrary location for now
+                    }
+                });
     }
 
     @FXML
