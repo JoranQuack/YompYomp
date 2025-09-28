f7857c8fac2024a961c855171ee85058e3d9413e
Joran Le Quellec <jle209@uclive.ac.nz>
2025-09-28 13:40:46 +1300

    refactor: Remove TODO and comment unused variables

--- app/src/main/java/seng202/team5/gui/ViewTrailController.java
+++ app/src/main/java/seng202/team5/gui/ViewTrailController.java
@@ -29,14 +29,14 @@ public class ViewTrailController extends Controller {
     private SearchService searchService;
 
     private WebEngine webEngine;
-    private JavaScriptBridge javaScriptBridge;
+    // private JavaScriptBridge javaScriptBridge;
     private JSObject javaScriptConnector;
 
     /**
      * Launches the screen with navigator
      *
-     * @param navigator screen navigator
-     * @param trail trail object to be displayed on screen
+     * @param navigator     screen navigator
+     * @param trail         trail object to be displayed on screen
      * @param searchService searchService
      */
     public ViewTrailController(ScreenNavigator navigator, Trail trail, SearchService searchService) {
@@ -103,7 +103,8 @@ public class ViewTrailController extends Controller {
             trailsRadiusTextField.setDisable(!selected); // disable if unchecked
             if (selected) {
                 trailsRadiusTextField.setText("20");
-                List<Trail> nearby = trailService.getNearbyTrails(trail, Integer.parseInt(trailsRadiusTextField.getText()), searchService.getAllTrails());
+                List<Trail> nearby = trailService.getNearbyTrails(trail,
+                        Integer.parseInt(trailsRadiusTextField.getText()), searchService.getAllTrails());
                 displayTrailsOnMap(nearby);
             } else {
                 // reset to just the current trail
@@ -126,30 +127,31 @@ public class ViewTrailController extends Controller {
         } else {
             translationLabel.setVisible(false);
         }
-        // TODO: add icon for map
 
         initMap();
     }
 
     /**
-     * Initialises the WebView loading in the appropriate html and initialising important communicator
+     * Initialises the WebView loading in the appropriate html and initialising
+     * important communicator
      * objects between Java and Javascript
      */
     private void initMap() {
         webEngine = trailMapView.getEngine();
         webEngine.setJavaScriptEnabled(true);
         webEngine.load(Controller.class.getResource("/html/map.html").toExternalForm());
-        //Forwards console.log() output from any javascript to info log
-        WebConsoleListener.setDefaultListener((view, message, lineNumber, sourceID) ->
-        System.out.printf(String.format("Map WebView console log line: %d, message : %s", lineNumber, message)));
+        // Forwards console.log() output from any javascript to info log
+        WebConsoleListener.setDefaultListener((view, message, lineNumber, sourceID) -> System.out
+                .printf(String.format("Map WebView console log line: %d, message : %s", lineNumber, message)));
 
         webEngine.getLoadWorker().stateProperty().addListener(
                 (ov, oldState, newState) -> {
                     // if javascript loads successfully
                     if (newState == Worker.State.SUCCEEDED) {
                         // set our bridge object
-                        JSObject window = (JSObject) webEngine.executeScript("window");
-                        // get a reference to the js object that has a reference to the js methods we need to use in java
+                        // JSObject window = (JSObject) webEngine.executeScript("window");
+                        // get a reference to the js object that has a reference to the js methods we
+                        // need to use in java
                         javaScriptConnector = (JSObject) webEngine.executeScript("jsConnector");
                         // call the javascript function to initialise the map
                         javaScriptConnector.call("initMap", trail.getLat(), trail.getLon());
@@ -171,6 +173,7 @@ public class ViewTrailController extends Controller {
 
     /**
      * displays the nearby trails as circle markers
+     *
      * @param trails the set of nearby trails
      */
     private void displayTrailsOnMap(List<Trail> trails) {
@@ -206,10 +209,12 @@ public class ViewTrailController extends Controller {
     /**
      * Handles the case where the View Trail Screen fails to load
      * by displaying an alert to the user
+     *
      * @param e the exception that occurred while loading the trail screen
      */
     @Override
     public void onLoadFailed(Exception e) {
-        showAlert(Alert.AlertType.ERROR, "Trail Card Failed To Load", "Loading selected trail failed, please close the application and try again.");
+        showAlert(Alert.AlertType.ERROR, "Trail Card Failed To Load",
+                "Loading selected trail failed, please close the application and try again.");
     }
 }
