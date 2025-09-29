776b5eeedd2b2ab26d02759ea781c096f3c6fe55
Joran Le Quellec <jle209@uclive.ac.nz>
2025-09-29 21:16:09 +1300

    feat: Add basic font styling and font loading

--- app/src/main/java/seng202/team5/gui/FXAppEntry.java
+++ app/src/main/java/seng202/team5/gui/FXAppEntry.java
@@ -3,6 +3,7 @@ package seng202.team5.gui;
 import java.util.Objects;
 import javafx.application.Application;
 import javafx.scene.image.Image;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 /**
@@ -19,6 +20,8 @@ public class FXAppEntry extends Application {
      */
     @Override
     public void start(Stage primaryStage) {
+        loadCustomFonts();
+
         Image icon = new Image(Objects.requireNonNull(getClass().getResourceAsStream("/images/YompYompIcon.png")));
         primaryStage.getIcons().add(icon);
         primaryStage.setTitle("YompYomp");
@@ -27,5 +30,20 @@ public class FXAppEntry extends Application {
         ScreenNavigator navigator = new ScreenNavigator(primaryStage);
         WelcomeController welcome = new WelcomeController(navigator);
         navigator.launchScreen(welcome, null);
+    }
+
+    /**
+     * Loads custom Quicksand fonts
+     */
+    private void loadCustomFonts() {
+        try {
+            Font.loadFont(getClass().getResourceAsStream("/fonts/Quicksand-Light.ttf"), 14);
+            Font.loadFont(getClass().getResourceAsStream("/fonts/Quicksand-Regular.ttf"), 14);
+            Font.loadFont(getClass().getResourceAsStream("/fonts/Quicksand-Medium.ttf"), 14);
+            Font.loadFont(getClass().getResourceAsStream("/fonts/Quicksand-SemiBold.ttf"), 14);
+            Font.loadFont(getClass().getResourceAsStream("/fonts/Quicksand-Bold.ttf"), 14);
+        } catch (Exception e) {
+            System.err.println("Warning: Could not load custom fonts: " + e.getMessage());
+        }
     }
 }
