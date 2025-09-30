820211b57d71cae33a02760b518f09f1f7b7c399
Sienna Robinson <sro179@uclive.ac.nz>
2025-09-30 16:02:52 +1300

    Updated controller to match fxml

--- app/src/main/java/seng202/team5/gui/LogTrailController.java
+++ app/src/main/java/seng202/team5/gui/LogTrailController.java
@@ -3,18 +3,17 @@ package seng202.team5.gui;
 import seng202.team5.data.DatabaseService;
 import seng202.team5.data.SqlBasedTrailRepo;
 import seng202.team5.data.SqlBasedTripRepo;
-import seng202.team5.models.TripLog;
+import seng202.team5.models.TrailLog;
 
 /**
  * Controller for the trip log screen
  */
 public class LogTrailController extends Controller {
 
-    private TripLog trip;
+    private TrailLog trailLog;
     private Controller lastController;
     private DatabaseService databaseService;
     private SqlBasedTrailRepo trailRepo;
-    private SqlBasedTripRepo tripRepo;
 
     /**
      * Launches the screen with the navigator
@@ -22,9 +21,9 @@ public class LogTrailController extends Controller {
      * @param navigator screen navigator
      * @param lastController controller of the last screen the user interacted with
      */
-    public LogTrailController(ScreenNavigator navigator, TripLog trip, Controller lastController) {
+    public LogTrailController(ScreenNavigator navigator, TrailLog trailLog, Controller lastController) {
         super(navigator);
-        this.trip = trip;
+        this.trailLog = trailLog;
         this.lastController = lastController;
         this.databaseService = new DatabaseService();
         this.trailRepo = new SqlBasedTrailRepo(databaseService);
