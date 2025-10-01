dc8332d7c5acee2168a2b41ed125ee89ba29886f
Sam Ladbrook <sla202@uclive.ac.nz>
2025-10-01 14:04:59 +1300

    added constructor and basic method for the log service class

--- app/src/main/java/seng202/team5/services/LogService.java
+++ app/src/main/java/seng202/team5/services/LogService.java
@@ -1,4 +1,36 @@
 package seng202.team5.services;
 
+import org.hsqldb.DatabaseManager;
+import seng202.team5.data.DatabaseService;
+import seng202.team5.data.SqlBasedTrailLogRepo;
+import seng202.team5.models.TrailLog;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class LogService {
+
+    private List<TrailLog> logs;
+    private int maxResults = 50;
+    private String currentSortBy = "name";
+
+    /**
+     * Creates LogService with database-backed filter options.
+     */
+    public LogService(DatabaseService databaseService) {
+        this.logs = new SqlBasedTrailLogRepo(databaseService).getAllTrailLogs();
+    }
+
+    /**
+     * Calculates the total number of pages required to display the currently
+     * list of logs.
+     */
+    public int getNumberOfPages() {
+        if (maxResults <= 0) {
+            return 1;
+        }
+        return (int) Math.ceil((double) logs.size() / maxResults);
+    }
+
+
 }
