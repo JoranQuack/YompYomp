c192a58aa87fac5e5da5fc92e53b6e99ec459c73
Sam Ladbrook <sla202@uclive.ac.nz>
2025-10-01 16:06:01 +1300

    added more new methods to go with those of the log book controller

--- app/src/main/java/seng202/team5/services/LogService.java
+++ app/src/main/java/seng202/team5/services/LogService.java
@@ -3,6 +3,7 @@ package seng202.team5.services;
 import org.hsqldb.DatabaseManager;
 import seng202.team5.data.DatabaseService;
 import seng202.team5.data.SqlBasedTrailLogRepo;
+import seng202.team5.data.SqlBasedTrailRepo;
 import seng202.team5.models.Trail;
 import seng202.team5.models.TrailLog;
 
@@ -13,14 +14,17 @@ import java.util.stream.Collectors;
 public class LogService {
 
     private List<TrailLog> logs;
+    private List<TrailLog> filteredLogs;
     private int maxResults = 50;
-    private String currentSortBy = "name";
+    private String currentSearchValue;
+    private SqlBasedTrailRepo trailRepo;
 
     /**
      * Creates LogService with database-backed filter options.
      */
     public LogService(DatabaseService databaseService) {
         this.logs = new SqlBasedTrailLogRepo(databaseService).getAllTrailLogs();
+        this.filteredLogs = logs;
     }
 
     /**
@@ -41,4 +45,39 @@ public class LogService {
         return logs.size();
     }
 
+    public Trail getTrail(int id) {
+        return new SqlBasedTrailRepo(new DatabaseService()).findById(id).get();
+    }
+
+    /**
+     * Updates trails based on current filters and sort settings.
+     */
+    public void updateLogs() {
+        String lower = currentSearchValue == null ? "" : currentSearchValue.toLowerCase();
+
+        filteredLogs = logs.stream()
+                .filter(t -> getTrail(t.getTrailId()).getName().toLowerCase().contains(lower))
+                .collect(Collectors.toList());
+    }
+
+    /**
+     * Gets a specific page of trails from the filtered results.
+     *
+     * @param page the page number (0-indexed)
+     * @return list of trails for the specified page
+     */
+    public List<TrailLog> getPage(int page) {
+        if (currentSearchValue != null) {
+            updateLogs();
+        }
+        int startIndex = page * maxResults;
+        return filteredLogs.stream()
+                .skip(startIndex)
+                .limit(maxResults)
+                .collect(Collectors.toList());
+    }
+
+    public void setCurrentQuery(String query) {
+        currentSearchValue = query;
+    }
 }
