dc805168583d4f0e8b2dfb546cde9699cef6709f
Jonny Chan <jch485@uclive.ac.nz>
2025-10-05 00:57:28 +1300

    Updated backend to go through API response properly

--- app/src/main/java/seng202/team5/services/WeatherAPI.java
+++ app/src/main/java/seng202/team5/services/WeatherAPI.java
@@ -1,21 +1,21 @@
 package seng202.team5.services;
 
-import seng202.team5.models.Weather;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
+import seng202.team5.models.Weather;
 
+import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
 public class WeatherAPI {
-
-    private static final String API_KEY = "your_api_key_here";
-    private static final String BASE_URL = "https://api.openweathermap.org/data/2.5/weather";
+    private static final String API_KEY = "API_KEY_HERE";
+    private static final String BASE_URL = "https://api.openweathermap.org/data/3.0/onecall";
 
     public static Weather getWeatherByCoords(double lat, double lon) {
         try {
-            String urlStr = String.format("%s?lat=%f&lon=%f&appid=%s&units=metric",
+            String urlStr = String.format("%s?lat=%f&lon=%f&exclude=minutely,hourly,alerts&units=metric&appid=%s",
                     BASE_URL, lat, lon, API_KEY);
             URL url = new URL(urlStr);
 
@@ -23,19 +23,25 @@ public class WeatherAPI {
             conn.setRequestMethod("GET");
 
             if (conn.getResponseCode() != 200) {
-                System.err.println("Error: " + conn.getResponseCode());
+                System.err.println("Error fetching weather: " + conn.getResponseCode());
                 return null;
             }
 
-            InputStreamReader reader = new InputStreamReader(conn.getInputStream());
-            JsonObject json = JsonParser.parseReader(reader).getAsJsonObject();
+            BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+            StringBuilder response = new StringBuilder();
+            String line;
+            while ((line = in.readLine()) != null) response.append(line);
+            in.close();
 
-            double temp = json.getAsJsonObject("main").get("temp").getAsDouble();
-            double tempMin = json.getAsJsonObject("main").get("temp_min").getAsDouble();
-            double tempMax = json.getAsJsonObject("main").get("temp_max").getAsDouble();
-            String description = json.getAsJsonArray("weather")
-                    .get(0).getAsJsonObject()
-                    .get("description").getAsString();
+            JsonObject json = JsonParser.parseString(response.toString()).getAsJsonObject();
+            JsonObject current = json.getAsJsonObject("current");
+            JsonObject today = json.getAsJsonArray("daily").get(0).getAsJsonObject();
+
+            double temp = current.get("temp").getAsDouble();
+            double tempMin = today.getAsJsonObject("temp").get("min").getAsDouble();
+            double tempMax = today.getAsJsonObject("temp").get("max").getAsDouble();
+            String description = current.getAsJsonArray("weather")
+                    .get(0).getAsJsonObject().get("description").getAsString();
 
             return new Weather(temp, tempMin, tempMax, description);
 
