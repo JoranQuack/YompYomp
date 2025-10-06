5ee00d189c37a6c2468b46627003a2b200a833d4
Jonny Chan <jch485@uclive.ac.nz>
2025-10-06 16:03:24 +1300

    Added initial new mock objects and setup for tests

--- app/src/test/java/seng202/team5/services/WeatherAPITest.java
+++ app/src/test/java/seng202/team5/services/WeatherAPITest.java
@@ -1,4 +1,21 @@
 package seng202.team5.services;
 
+import io.cucumber.java.Before;
+import org.junit.jupiter.api.BeforeEach;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.net.HttpURLConnection;
+
 public class WeatherAPITest {
+    @Mock
+    private WeatherAPI weatherAPI;
+
+    @Mock
+    private HttpURLConnection mockConnection;
+
+    @BeforeEach
+    public void setup() {
+        WeatherAPI weatherAPI = new WeatherAPI();
+    }
 }
